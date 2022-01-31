4124e380f3634be027e1b0bb0a0bc4389045c196
Sarika Soman <sarikasomasekharan@gmail.com>
2022-01-31 13:57:26 +0100

    Add docstrings to functions

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ print('Hello World')
 
 import numpy as np
 def circumference(r):
+    """ calculate the circumference of a circle
+    """
     return 2*np.pi*r
 def circ_area(r):
+    """ calculate the area of a circle
+    """
     return np.pi * r**2
