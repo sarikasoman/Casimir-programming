d0427a192c647ef61c4577a8e1e88310a8586374
Sarika Soman <sarikasomasekharan@gmail.com>
2022-01-31 11:55:02 +0100

    Import test.py and call functions

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-print('Hello World')
+from test import *
+
+print('This is script.py')
+print('I print stuff.')
+
+print('circumference of a circle with radius 10 is {}'.format(circumference(10)))
+print('area of a circle with radius 10 is {}'.format(circ_area(10)))
